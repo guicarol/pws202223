b953fafc9d4397e1f0e092e3c19a0c68cf4b9528
guicarol <2202410@my.ipleiria.pt>
2023-05-26 21:03:53 +0100

    .

---
+++ startup/config.php
@@ -0,0 +1,16 @@
+<?php
+require 'vendor/autoload.php';
+
+define('APP_NAME', 'Credit App');
+define('INVALID_ACCESS_ROUTE', 'c=auth&a=index');
+
+
+ActiveRecord\Config::initialize(function($cfg)
+{
+    $cfg->set_model_directory('./models');
+    $cfg->set_connections(
+        array(
+            'development' => 'mysql://root@localhost/appdb',
+        )
+    );
+});
