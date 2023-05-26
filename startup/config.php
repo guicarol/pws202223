e5b45589411d0e1861f7be7fd9bf0852a29e1289
guicarol <2202410@my.ipleiria.pt>
2023-05-26 21:06:51 +0100

    Update config.php

--- startup/config.php
+++ startup/config.php
@@ -10,7 +10,7 @@ ActiveRecord\Config::initialize(function($cfg)
     $cfg->set_model_directory('./models');
     $cfg->set_connections(
         array(
-            'development' => 'mysql://root@localhost/appdb',
+            'development' => 'mysql://root@localhost/projetopws',
         )
     );
 });
